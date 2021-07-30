af2c9bdaad2dca873e09cf9114952cee1ed731f5
klelostec <kevin.lelostec@gmail.com>
2021-07-30 14:51:06 +0200

    Enhance user and profile management.

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', null, [
+                'empty_data' => ''
+            ])
+            ->add('lastname', null, [
+                'empty_data' => ''
+            ])
+            ->add('email', EmailType::class, [
+                'empty_data' => ''
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'validation_groups' => ['Default', 'profile']
+        ]);
+    }
+}
