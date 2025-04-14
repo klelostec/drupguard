838e0d5f1e82fb6caa3314f20ebdcc27e33a3bdd
YasmineSAI <saidiyasmine2001@gmail.com>
2025-04-14 14:04:44 +0200

    Fix project state and plugin state.

--- src/Plugin/Service/Type/Analyse/ComposerAudit.php
+++ src/Plugin/Service/Type/Analyse/ComposerAudit.php
@@ -15,6 +15,7 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 use function Symfony\Component\Translation\t;
 
+
 #[TypeInfo(
     id: 'composer_audit',
     name: 'Composer audit',
@@ -92,9 +93,24 @@ class ComposerAudit extends Analyse
             }
             $item->setState($itemState);
             $reportAnalyse->addItem($item);
-            $state = min($state, $itemState);
+            if ($itemState->value < $state->value) {
+                $state = $itemState;
+            }
+            dump($name, $itemState->name, $state->name);
         }
+
+        $details = [];
+
+        foreach ($reportAnalyse->getItems() as $item) {
+            if ($item->getDetail()) {
+                $details[] = $item->getName() . ': ' . $item->getDetail();
+            }
+        }
+        if (!empty($details)) {
+            $reportAnalyse->setDetail(implode("\n\n", $details)); 
+            }
         $reportAnalyse->setState($state);
+        //$this->logger->debug('ComposerAudit report state: ' . $reportAnalyse->getState()->name);
         return $reportAnalyse;
     }
 }
